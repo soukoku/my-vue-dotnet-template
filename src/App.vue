0dbdee344e43b875d1ee959b68fbd98cda28b350
Eugene Wang <8755753+soukoku@users.noreply.github.com>
2023-11-01 22:57:02 -0400

    Added tailwind form/typography plugins.

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <div class="wrapper">
+    <div class="prose">
       <HelloWorld msg="You did it!" />
 
       <nav>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
+import typography from '@tailwindcss/typography'
+import forms from '@tailwindcss/forms'
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: [typography, forms]
 }
