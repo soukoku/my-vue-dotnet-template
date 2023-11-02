af1bd34484d812a23be2b4457c1e1e503cdb6dc2
Eugene Wang <8755753+soukoku@users.noreply.github.com>
2023-11-01 23:04:18 -0400

    Use inter font.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 import typography from '@tailwindcss/typography'
 import forms from '@tailwindcss/forms'
+import defaultTheme from 'tailwindcss/defaultTheme'
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans]
+      }
+    }
   },
   plugins: [typography, forms]
 }
